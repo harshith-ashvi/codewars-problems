31747a1d7062b19059ec8751803dd108957fcfa7
harshith <harshith.ashvi@gmail.com>
2022-01-13 23:09:26 +0530

    Solved calculateAge problem in 8 kyu

---
+++ 8 kyu/distinct.js
@@ -0,0 +1,10 @@
+// Remove duplicates from list
+
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// My Solution
+function distinct(a) {
+    return [...new Set(a)];
+}
