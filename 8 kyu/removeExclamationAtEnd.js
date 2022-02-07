8f2eba747c253be57bd16a599a59ac037e97e523
harshith <harshith.ashvi@gmail.com>
2022-02-07 22:49:45 +0530

    solve removeExclamationAtEnd problem in 8 kyu

---
+++ 8 kyu/removeExclamationAtEnd.js
@@ -0,0 +1,25 @@
+// Exclamation marks series #1: Remove an exclamation mark from the end of string
+
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi!!"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+// Note
+// Please don't post issue about difficulty or duplicate.
+
+// My Solution
+function remove (string) {
+    return string[string.length - 1] === "!"? string.slice(0, string.length - 1):string;
+}
+
+// Other's Solution
+const remove = s => s.replace(/!$/, '');
+
+function remove(s) {
+    return s.endsWith('!') ? s.slice(0, -1) : s;
+}
