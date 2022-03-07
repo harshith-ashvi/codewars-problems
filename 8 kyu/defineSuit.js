943509b84e028319661502ae9e753ea9ca24b109
harshith <harshith.ashvi@gmail.com>
2022-03-07 22:37:51 +0530

    solve defineSuit problem in 8 kyu

---
+++ 8 kyu/defineSuit.js
@@ -0,0 +1,54 @@
+// Define a card suit
+
+// You get any card as an argument. Your task is to return a suit of this card.
+
+// Our deck (is preloaded):
+
+// deck = ['2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣','A♣',
+//         '2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦','A♦',
+//         '2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥','A♥',
+//         '2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠','A♠'];
+
+
+// ('3♣') -> return 'clubs'
+// ('3♦') -> return 'diamonds'
+// ('3♥') -> return 'hearts'
+// ('3♠') -> return 'spades'
+
+//My Solution
+function defineSuit(card) {
+    return card.includes('♣')? 'clubs':card.includes('♦')? 'diamonds':card.includes('♥')? 'hearts':'spades'
+}
+
+//Other's Solution
+function defineSuit(card) {
+    const s = {
+      "♣": "clubs",
+      "♠": "spades",
+      "♦": "diamonds",
+      "♥": "hearts"
+    }
+    return s[card.charAt(card.length - 1)]
+}
+
+function defineSuit(card) {
+    return {
+      '♣' : 'clubs',
+      '♦' : 'diamonds',
+      '♥' : 'hearts',
+      '♠' : 'spades'
+    }[card.substr(-1)]
+}
+
+function defineSuit(card) {
+    if(card.includes('♥')) return 'hearts'
+    if(card.includes('♦')) return 'diamonds'
+    if(card.includes('♣')) return 'clubs'
+    if(card.includes('♠')) return 'spades' 
+}
+
+function defineSuit(card) {
+    return {'♣': 'clubs', '♠': 'spades', '♦': 'diamonds', '♥': 'hearts'}[card.slice(-1)];
+}
+
+defineSuit=([a,b,c])=>({"♣":'clubs',"♠":'spades',"♦":'diamonds',"♥":'hearts'})[c||b]
