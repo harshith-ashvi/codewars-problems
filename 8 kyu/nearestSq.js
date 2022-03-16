4f65e0861ec117a7d4e5e1ce0fbaf9676244418d
harshith <harshith.ashvi@gmail.com>
2022-03-16 20:05:26 +0530

    solve nearstSq problem in 8 kyu

---
+++ 8 kyu/nearestSq.js
@@ -0,0 +1,8 @@
+// Find Nearest square number
+
+// Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+// My Solution
+function nearestSq(n){
+    return Math.round(n ** (1/2)) ** 2
+}
